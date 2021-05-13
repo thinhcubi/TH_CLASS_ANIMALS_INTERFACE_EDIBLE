41502592bd5cdf2a2bd9796203ab3d822f799c52
thinhcubi <thinhcubi27@gmail.com>
2021-05-13 20:29:13 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include_once ('Animals/Chicken.php');
+include_once ('Animals/Tiger.php');
+include_once ('Fruits/Apple.php');
+include_once ('Fruits/Orange.php');
+
+echo('---- Animals<br>');
+$animals[0] = new Tiger();
+$animals[1] = new Chicken();
+foreach ($animals as $animal){
+    echo $animal->makeSound().' ';
+    if ($animal instanceof Chicken) {
+        echo $animal->howToEat(). ' ';
+    }
+}
+
+echo ('---- Fruits<br>');
+$fruits[0] = new Apple();
+$fruits[1] = new Orange();
+
+foreach ($fruits as $fruit) {
+    echo $fruit->howToEat().'<br>';
+}
